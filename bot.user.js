7781481726c0c7042fcfe0e826c9b8da22b307a9
r0n1xx <kastratialbin@gmail.com>
2016-01-18 20:36:18 +0100

    bot.user.js
    NEW WEBSITE

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agariopvp.biz/
 // @version     3.645
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
